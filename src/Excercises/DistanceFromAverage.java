e3b21e7ec6dd096f7d613cd14c11a6712a2fd436
Logan Gosch <logan.gosch.temp@gmail.com>
2019-10-04 09:49:22 -0500

    updated chapter 8

--- src/Excercises/DistanceFromAverage.java
+++ src/Excercises/DistanceFromAverage.java
@@ -6,26 +6,33 @@ public class DistanceFromAverage {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        double[] twentyDoubles = new double[20];
-        double avg;
-        double total = 0;
         int i = 0;
         int userEntry = 0;
+        int count = 0;
+        double[] twentyDoubles = new double[20];
+        double avg = 0;
+        double total = 0;
 
-        while(userEntry != 99999){
-
-            System.out.println("Enter a number: 99999 to stop >>>");
+        while(userEntry != 99999)
+        {
+            count++;
+            System.out.println("Enter Number " + count + " : 99999 to Stop >>>");
             userEntry = input.nextInt();
 
             if(userEntry != 99999){
                 i++;
                 twentyDoubles[i] = userEntry;
                 total = total + userEntry;
+                avg = total/i;
             }
             else
                 System.out.println("Good Bye SIR!");
         }
+        System.out.println("The average of your entries is " + avg);
 
-        System.out.println("The average of your entries is " + total/i);
+        for(int j = 0; j < twentyDoubles.length; j++){
+            
+        }
+
     }
 }
