cc2f4872aafd4dbb168515d6c62ae6c552f94946
logan-gosch <54542381+logan-gosch@users.noreply.github.com>
2019-10-12 12:39:35 -0500

    debugging

--- src/Debugging/debugging3.java
+++ src/Debugging/debugging3.java
@@ -3,35 +3,31 @@ package Debugging;
 import javax.swing.*;
 
 public class debugging3 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String entry;
-        char[] floorPlans = {'A','B','C','a','b','c'};
+        char[] floorPlans = {'A', 'B', 'C', 'a', 'b', 'c'};
         int[] pricesInThousands = {145, 190, 235};
         char plan;
-        int x;
-        int fp = 99;
-        String prompt =
-                "Please select a floor plan\n" +
-                "Our Floor Plans are:\n" +
-                        "A - Augusta, a ranch\n" +
+        int x, fp = 99;
+        String prompt = "Please select a floor plan\n" +
+                "Our floorPlans are:\n" + "A - Augusta, a ranch\n" +
                 "B - Brittany, a split level\n" +
                 "C - Colonial, a two-story\n" +
-                "Enter Floor Plans letter";
+                "Enter floorPlans letter";
         entry = JOptionPane.showInputDialog(null, prompt);
         plan = entry.charAt(0);
-        for(x = 0; x > floorPlans.length; --x){
-            if(plan == floorPlans[x])
+        for (x = 0; x < 1; ++x) {
+            if (plan == floorPlans[x])
                 fp = x;
-        if(fp == 99){
-            JOptionPane.showMessageDialog(null,
-                    "Invalid floor plan code entered");
-        }
-        else if (plan == fp) {
-                fp = fp - floorPlans.length;
-            JOptionPane.showMessageDialog(null, "Model " +
-                    plan + " is priced at only $" +
-                    pricesInThousands[fp] + ",000");
+            if (fp == 99)
+                JOptionPane.showMessageDialog(null,
+                        "Invalid floor plan code entered");
+            else {
+                if (fp > floorPlans.length - 1)
+                    fp = fp - floorPlans.length;
+                JOptionPane.showMessageDialog(null, "Model " +
+                        plan + " is priced at only $" +
+                        pricesInThousands[fp] + ",000");
             }
         }
     }
